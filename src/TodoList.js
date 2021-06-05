06feab3068a1c1f85c1f9d7466c61ad35bdee7b0
Martin Heide <martin.heide@gmx.net>
2021-06-05 23:47:38 +0200

    Semi-persistent todo list

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,39 +3,86 @@ import React from 'react';
 class TodoList extends React.Component {
   constructor(props) {
     super(props);
-    console.log('todos', props.todos);
+    //console.log('todos', props.todos);
 
-    const todoList = props.todos.map(text => {
-      return {text, done: false};
-    })
+    // const todoList = props.todos.map(text => {
+    //   return {text, done: false};
+    // })
 
     // const todoMap = new Map();
     // for (const todo of props.todos) {
     //   todoMap.set(todo, !!JSON.parse(localStorage.getItem(`todo_{todo}`)));
     // }
-
-    this.state = {
-      todoList
+    
+    try {      
+      this.state = JSON.parse(localStorage.getItem('todos'));
+    } catch (e) {
+      console.error('Failed to parse todos from localStorage', e);
     }
+
+    this.state = this.state || {
+      todoList: [],
+      newTodoText: '',
+      newTodoId: 1
+    }
+    console.log(this.state);
+
+    this.addTodo = this.addTodo.bind(this);
+    this.updateNewTodoText = this.updateNewTodoText.bind(this);
+  }
+
+  save() {
+    this.setState((state, props) => {
+      console.log('Saving todos', state);
+      localStorage.setItem('todos', JSON.stringify(state));
+      return {};
+    });
+  }
+
+  addTodo() {
+    this.setState((state, props) => ({
+      todoList: [{
+        id: state.newTodoId,
+        text: state.newTodoText,
+        done: false
+      }].concat(state.todoList),
+      newTodoId: state.newTodoId + 1
+    }));
+    this.save();
+  }
+
+  updateNewTodoText(event) {
+    this.setState({newTodoText: event.target.value});
+    console.log({newTodoText: event.target.value});
   }
 
   render() {
     const todos = this.state.todoList.map(todo => {
+      let del = () => {
+        this.setState((state, props) => ({
+          todoList: state.todoList.filter((td) => td !== todo)
+        }));
+        this.save();
+      }
+
       return (
-        <label key={todo.text} style={{display: "block"}}>
-          <input type="checkbox"></input>
-          {todo.text}
-        </label>
+        <div key={todo.id} style={{display: "block"}}>
+          <label>
+            <input type="checkbox"></input>
+            {todo.text}
+          </label>&nbsp;
+          <button onClick={del}>✗</button>
+        </div>
       );
     })
 
     return (
       <div style={{textAlign: "left"}}>
-        {todos}
         <div>
-          <button>+</button>
-          <input></input>
+          <button onClick={this.addTodo}>+</button>
+          <input onChange={this.updateNewTodoText}></input>
         </div>
+        {todos}
       </div>
     );
   }
